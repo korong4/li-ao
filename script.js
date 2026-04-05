8d7b27e195d6dea9cb953e0734af2ccc46277001
Gabriel Morais de Jesus <gabrielmoraisdejesus1@gmail.com>
2026-04-05 12:46:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ function alterarFundo(){
     let cor = document.body.style.backgroundColor;
     if (cor ===  "black"){
         document.body.style.backgroundColor = "white";
+        document.querySelector('p').style.color = "black";
     } 
     else{
         document.body.style.backgroundColor = "black";
+        document.querySelector('p').style.color = "white";
     } 
 }
 
 let n = 25;
-
 for (let i=0;i<n;i++){
     let linha = "";
     for (let j=0;j<n;j++){
