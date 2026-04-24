d2a85ca7b8824b13ecfd9d06ba10e5b0900dba9b
Gabriel Morais de Jesus <gabrielmoraisdejesus1@gmail.com>
2026-04-24 14:49:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,18 @@ for (let i=0;i<n;i++){
     console.log(linha);
 }
 
-const formulario = document.querySelector(".formulario");
+const formulario = document.querySelector(".formulario"); /* essa linha cria uma variavel constante (não pode mudar ao longo do codigo), que esta sendo atribuido o  
+  priemeiro elemento com class 'formulario' dentro do codigo HTML  */
 
-formulario.addEventListener('submit',function(evento){
-    evento.preventDefalut();
-    const novoTitulo = documento.getElementsById('raca').value;
-    const novoImagem = documento.getElementsById('imagem').value;
-    const novoApp = documento.getElementsById('app').value;
+formulario.addEventListener('submit',function(evento){   // adiciona um evento ao formulário que dispara quando ele é enviado
+    evento.preventDefalut(); // impede o comportamento padrão 
+    const novoTitulo = documento.getElementsById('raca').value; // pega o elemento do HTML com ID "raca"
+    const novoImagem = documento.getElementsById('imagem').value; // pega o elemento do HTML com ID "imagem"
+    const novoApp = documento.getElementsById('app').value; // pega o elemento do HTML com ID "app"
+
+    const novo = { // essa estrutura está criando um obejto com 3 atributos
+        titilo : novoTitulo,
+        imagem : novoImagem,
+        app : novoApp
+    };
 })
